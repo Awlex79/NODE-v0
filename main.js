08051c96b6cfb663c94a8affcb278986986951dc
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2019-01-06 23:28:05 -0600

    ++

--- main.js
+++ main.js
@@ -7,3 +7,54 @@ var me = {
 };
 
 console.log(me.greeting());
+
+var arr = [10, 20, 30, 40];
+for (let zZz of arr) {
+    //let newArr = arr.push(zZz++);
+    //console.log(newArr); 
+    zZz++;
+    console.log(zZz);
+}
+
+console.log(arr);
+
+
+/* FUNCTION GENERATOR */
+
+function* idMaker() {
+    var index = 0;
+    while (index < 4)
+    yield index++;
+}
+
+var gen = idMaker();
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+
+const person = {
+    name: "Max",
+    age: 29,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+     
+    }
+}
+
+const copiedPerson = {...person};
+console.log(copiedPerson);
+
+const hobbies = ['Sports', 'Cooking'];
+//for (let hobby of hobbies) {
+//  console.log(hobby);
+//    }
+// console.log(hobbies.map(hobby => 'Hobby:' + hobby));
+// console.log(hobbies);
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+    
+
+
